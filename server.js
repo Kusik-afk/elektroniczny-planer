a50f74b1c8c90fa050114b23fe459467b5a68e39
Kusik-afk <vikadudkevych8@gmail.com>
2025-12-10 18:51:55 +0100

    MongoDB Atlas i Mongoose

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 //1. Importujemy bibliotekę Express
+require('dotenv').config();//ładujemy tajne zmienne z pliku .env
 const express = require('express');
+const mongoose = require('mongoose');//importujemy Mongoose
 const path = require('path');
 
 //2. Tworzymy aplikację
 const app = express();
 
+//Łączymy się z bazą danych
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Połączono z bazą MongoDB!'))
+    .catch(err => console.error('Błąd połączenia z bazą:', err));
+
 //3. Mówimy serwerowi, żeby serwował pliki z naszego folderu
 app.use(express.static(path.join(__dirname, '')));
+//Pozwala serwerowi rozumieć dane wysyłane w formacie JSON
+app.use(express.json());
 
 //4. Definiujemy trasę dla strony głównej
 app.get('/', (req, res) => {
